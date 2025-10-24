c8c8a1152e4d8f04447b6b8a22ade8098815a2dd
Arnav <arnav.n.karnik@gmail.com>
2025-10-24 15:35:19 -0500

    changes

--- running/vite.config.js
+++ running/vite.config.js
@@ -1,7 +1,8 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+// vite.config.js
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/Running/',
+  base: '',  // replace 'repo-name' with your GitHub repo name
 });
