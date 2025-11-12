66a15abd42a216cc07eeae0da68ad7fec5d7ffd5
Arnav <arnav.n.karnik@gmail.com>
2025-11-11 21:01:57 -0600

    try and see if it works

--- running/vite.config.js
+++ running/vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '',  // replace 'repo-name' with your GitHub repo name
+  // base: '/Running/',  // replace 'repo-name' with your GitHub repo name
 });
